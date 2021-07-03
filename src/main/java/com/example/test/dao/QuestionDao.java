a5bf71b3999512fae82331594d781c50d4235e49
lingz_glb <920314957@qq.com>
2021-07-03 10:15:41 +0800

    修改参数类型

--- src/main/java/com/example/test/dao/QuestionDao.java
+++ src/main/java/com/example/test/dao/QuestionDao.java
@@ -1,15 +1,12 @@
 package com.example.test.dao;
 
 import java.util.List;
-import java.util.Map;
 
-import com.example.test.entity.Paper;
 import com.example.test.entity.Question;
-import com.example.test.entity.Type;
 import org.apache.ibatis.annotations.Param;
 
 public interface QuestionDao extends BaseDao<Question>{
-    public List<Question> createPaper(@Param("ch_no")List<Integer> ch_no,
-                                      @Param("type")List<String> type,
-                                      @Param("paperDif")int paperDif);
+    List<Question> createPaper(@Param("ch_no") List<String> ch_no,
+                               @Param("type") List<String> type,
+                               @Param("paperDif") int paperDif);
 }
